60654b28868ef59aafc9f693235a2bae04460ad2
iamEJ <jukneviciute185@gmail.com>
2020-10-20 16:59:54 +0300

    setup contextApi (stateProvider and reducer)

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Login from "./Components/Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Components/Player";
+import { useStateValue } from "./Components/StateProvider";
 
 const spoti = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user, token }, dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -16,15 +17,23 @@ function App() {
     let _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
       spoti.setAccessToken(_token);
       spoti.getMe().then((user) => {
-        console.log("Hello", user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
   }, []);
 
-  return <div className="app">{token ? <Player /> : <Login />}</div>;
+  return (
+    <div className="app">{token ? <Player spotify={spoti} /> : <Login />}</div>
+  );
 }
 
 export default App;
--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./css/Player.css";
 
-function Player() {
+function Player({ spotify }) {
   return (
     <div className="player">
       <h1>I am the player</h1>
