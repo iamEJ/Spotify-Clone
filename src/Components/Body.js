b08c402c0a48fae6701c14e2a655f064e0deead8
iamEJ <jukneviciute185@gmail.com>
2020-10-21 18:36:23 +0300

    add songRow component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -5,6 +5,7 @@ import { useStateValue } from "./StateProvider";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "./SongRow";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useStateValue();
@@ -27,9 +28,9 @@ function Body({ spotify }) {
           <MoreHorizIcon />
         </div>
 
-        {/* {discover_weekly?.tracks.items.map((item) => (
+        {discover_weekly?.tracks.items.map((item) => (
           <SongRow track={item.track} />
-        ))} */}
+        ))}
       </div>
     </div>
   );
